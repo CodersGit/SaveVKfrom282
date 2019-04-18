022af4f35be406eae5190a6b218531d7378cf58a
Sergey Zhirov <polarwolf@cscleague.org>
2019-04-18 12:00:43 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <TitleBar/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import TitleBar from "./components/TitleBar.vue";
+
+export default Vue.extend({
+  name: "app",
+  components: {
+    TitleBar
+  }
+});
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  vertical-align: baseline;
+}
+html {
+  box-sizing: border-box;
+  background: rgba(70, 128, 194, 0.4) !important;
+  border: none
+}
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+html, body {
+  height: 100%;
+}
+body {
+  border: 1px solid #48545c;
+  overflow-y: hidden;
+}
+#app {
+  font-family: "Segoe UI", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  background: rgba(72, 84, 92, 1)!important;
+}
+</style>
---
+++ src/components/WindowButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="button" @click="this.dispatchAction">
+    <span v-html="text"></span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import {Payload} from 'vuex';
+
+export default Vue.extend({
+  name: "WindowButton",
+  props: {
+    text: { required: true },
+    action: { required: true },
+  },
+  methods: {
+    dispatchAction() {
+      this.$store.dispatch(this.action as Payload)
+    }
+  }
+});
+</script>
+
+<style scoped>
+.button {
+  grid-row: 1 / span 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  user-select: none;
+  cursor: default;
+  opacity: 0.8;
+}
+</style>
